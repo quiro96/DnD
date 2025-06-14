3b7377cc4b8689ec405479957d85d748acb34be9
quiro96 <62141217+quiro96@users.noreply.github.com>
2025-06-14 22:48:28 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // js/config.js
 
 export const CELL_SIZE = 50;
-export const MAX_ROUNDS = 20;
+export const MAX_ROUNDS = 100;
 export const MOVEMENT_ANIMATION_SPEED_MS = 100;
 export const FLOATING_TEXT_DURATION_MS = 1500;
 export const DEATH_FADE_DURATION_MS = 1000;
